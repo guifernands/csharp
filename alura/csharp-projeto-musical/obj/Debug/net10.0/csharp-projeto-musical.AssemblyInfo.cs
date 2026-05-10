bb535855b4f2a8e8bcf4be0b1aa67b5a680dcb41
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2026-05-10 20:47:26 -0300

    teste

--- alura/csharp-projeto-musical/obj/Debug/net10.0/csharp-projeto-musical.AssemblyInfo.cs
+++ alura/csharp-projeto-musical/obj/Debug/net10.0/csharp-projeto-musical.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-projeto-musical")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1495d211fbd9b3aca13b9d33cc94cb2159c9ba31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+985d14f2712d4c7afb30e8d340be7d0d3dda7150")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-projeto-musical")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-projeto-musical")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
